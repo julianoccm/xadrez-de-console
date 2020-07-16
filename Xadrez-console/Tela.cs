807b6674812d26c67727590e6e1ea54cebd6491d
JulianoCCMoreira <julianocolerecmoreira@gmail.com>
2020-07-16 18:09:43 -0300

    Criado um metodo para imprimir o tabuleiro.

---
+++ Xadrez-console/Tela.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using tabuleiro;
+
+namespace Xadrez_console
+{
+    class Tela
+    {
+        public static void imprimirTabuleiro(Tabuleiro tab)
+        {
+            for (int i = 0; i < tab.linhas; i++)
+            {
+                for (int j = 0; j < tab.colunas; j++)
+                {
+                    if (tab.peca(i, j) == null)
+                    {
+                        Console.Write("- ");
+                    }
+                    else
+                    {
+                        Console.Write(tab.peca(i, j) + " ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
