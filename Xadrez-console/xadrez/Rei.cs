5e01f70b8e997f27f73066ab7b82e556a530c5f4
JulianoCCMoreira <julianocolerecmoreira@gmail.com>
2020-07-16 18:12:49 -0300

    Criada duas peças: Rei e Torre.

---
+++ Xadrez-console/xadrez/Rei.cs
@@ -0,0 +1,16 @@
+﻿using tabuleiro;
+
+namespace xadrez
+{
+    class Rei : Peca
+    {
+        public Rei(Tabuleiro tab, Cor cor) : base(tab, cor)
+        {
+        }
+
+        public override string ToString()
+        {
+            return "R";
+        }
+    }
+}
