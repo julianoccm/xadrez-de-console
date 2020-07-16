794b165e792cfeaf186b35e1ed3d8cf6b32a6e63
JulianoCCMoreira <julianocolerecmoreira@gmail.com>
2020-07-16 18:05:10 -0300

    Criada a pasta Tabuleiro e a classe Posição

---
+++ Xadrez-console/tabuleiro/Posicao.cs
@@ -0,0 +1,19 @@
+﻿namespace tabuleiro
+{
+    class Posicao
+    {
+        public int Linha { get; set; }
+        public int Coluna { get; set; }
+
+        public Posicao(int linha, int coluna)
+        {
+            Linha = linha;
+            Coluna = coluna;
+        }
+
+        public override string ToString()
+        {
+            return $"{Linha}, {Coluna}.";
+        }
+    }
+}
